d154088ceff12864d42f7b679106bce65400cf33
18928961614 <14696387+zhong-yuanhao-and-dunyi@user.noreply.gitee.com>
2024-08-26 23:30:16 +0800

    11

--- minigame/maze/script.js
+++ minigame/maze/script.js
@@ -130,16 +130,23 @@ document.addEventListener('keydown', (e) => {
             const fogY = newTop + player.clientHeight / 2;
             if (newTop >= targetArea.top && newTop <= targetArea.top + targetArea.height &&
                 newLeft >= targetArea.left && newLeft <= targetArea.left + targetArea.width) {
-            
+             alert('获得成就“迷宫大师”');
                 clearInterval(countdownInterval);
-                    // var index = window.localStorage.userid;
-					// var array = JSON.parse(window.localStorage.userArr);
+                    
 					
-					// if (totalScore2 == 2) 
-					// 	array[index].achi2 = 1;
+                    var index = window.localStorage.userid;
+					var array = JSON.parse(window.localStorage.userArr);
+                    array[index].achi2 = 1;
+					console.log('index:', index);  // 确认index的值
+console.log('array:', array);  // 确认array是否被正确解析
+console.log('array[index]:', array[index]);  // 确认 array[index] 是否有效
+              console.log('array[index].achi2:', array[index].achi2); 
+
+					
+              window.localStorage.userArr = JSON.stringify(array);
 						
 					
-                alert('获得成就“迷宫大师”');
+               
                 window.location.href = "next_page.html"; // 跳转到下一个页面
                 }
             // fog.style.clipPath = `circle(${radius}px at ${fogX}px ${fogY}px)`;
