c0deb5c146c6550fbc696687b9329ad4cfdefb26
18928961614~ <14696387+zhong-yuanhao-and-dunyi@user.noreply.gitee.com>
2024-09-02 01:53:43 +0800

    chapter1打印效果统一

--- js/player1.js
+++ js/player1.js
@@ -252,23 +252,13 @@ updateAdjacentPieces(x, y) {
       dialogText.style.textShadow = "2px 2px 4px #000000"; // 文本阴影
       dialogText.style.lineHeight = "1.5"; // 行高
       
-      function typeDialogue() {
-        
-        if (charIndex < dialogues[currentDialogue].text.length) {
-          dialogText.innerText += dialogues[currentDialogue].text.charAt(charIndex);
-          charIndex++;
-          setTimeout(typeDialogue, typingSpeed);
-        } else {
-          currentDialogue++;
-          charIndex = 0;
-        }
-      }
+      
 
       function showNextDialogue() {
         if (currentDialogue < dialogues.length) {
-          dialogText.innerText = "";
+          dialogText.innerText = dialogues[currentDialogue].text;
           lailaImage.src = dialogues[currentDialogue].image;
-          typeDialogue();
+          currentDialogue++;
         } else {
           document.body.removeChild(dialogBox);
           document.getElementById("gameCanvas").style.display = "block";
@@ -545,23 +535,14 @@ function createDialogueBox(dialogues) {
   dialogBox.appendChild(dialogText);
   document.body.appendChild(dialogBox);
 
-  function typeDialogue() {
-    if (charIndex < dialogues[currentDialogue].text.length) {
-      dialogText.innerText += dialogues[currentDialogue].text.charAt(charIndex);
-      charIndex++;
-      setTimeout(typeDialogue, typingSpeed);
-    } else {
-      currentDialogue++;
-      charIndex = 0;
-    }
-  }
+  
 
   function showNextDialogue() {
     if (currentDialogue < dialogues.length) {
       // 更新图片和文本内容
       charImage.src = dialogues[currentDialogue].image;
-      dialogText.innerText = "";
-      typeDialogue();
+      dialogText.innerText = dialogues[currentDialogue].text;
+      currentDialogue++;
     } else {
       document.body.removeChild(dialogBox);
       document.getElementById("gameCanvas").style.display = "block";
